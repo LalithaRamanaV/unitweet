ea9470298cb0289b1a527ff5de2c5829264fc572
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-11 12:51:55 +0530

    added post section of feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,29 @@
 import { SparklesIcon } from '@heroicons/react/outline'
 import React from 'react'
 import Input from './Input'
+import Post from './Post'
 
 function Feed() {
+    const posts = [
+        {
+            id:"1",
+            name:"Lalitha Vadavalli",
+            username:"lalitharamanaV",
+            userImg:"https://avatars.githubusercontent.com/u/58090261?v=4",
+            img:"https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1274&q=80",
+            text:"This is stunning",
+            timestamp:"1 hour ago"
+        },
+        {
+            id:"2",
+            name:"Lalitha ",
+            username:"lalithaV",
+            userImg:"https://avatars.githubusercontent.com/u/58090261?v=4",
+            img:"https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+            text:"Nice view",
+            timestamp:"2 hours ago"
+        }
+    ]
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
      <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -12,6 +33,9 @@ function Feed() {
      </div>
     </div>
     <Input/>
+    {posts.map((post)=> (
+        <Post key={post.id} post={post}/>
+    ))}
     </div>
   )
 }
