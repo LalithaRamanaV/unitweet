4f24c6ecf38d84a96ed8e4f597b0878ff72f5a1f
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-11 21:18:22 +0530

    google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -9,5 +10,21 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-})
 
+  pages: {
+    signIn: "/auth/signin",
+  },
+
+  secret: process.env.SECRET,
+
+  callbacks: {
+    async session({ session, token }) {
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+});
