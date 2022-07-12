b94bb2fa62b67e4cd0fc3d80e8c8348c6675359d
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-12 18:25:39 +0530

    added loading effect and database to firebase

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp ,getApp,getApps} from "firebase/app";
-import {getFirestore} from "/firebase/firestore";
-import {getStorage} from "/firebase/storage";
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,13 +5,13 @@ import Image from 'next/image'
 
 function signin({providers}) {
   return (
-    <div className="flex justify-center mt-20 space-x-4">
-      <img className="hidden object-cover md:w-60 md:h-80 md:inline-flex" src="https://melissataub.com/wp-content/uploads/2019/07/Twitter-Mockup-crop2.png" alt="logo"/>
+    <div className="flex justify-center mt-20 space-x-6">
+      <img className="hidden object-cover md:w-60 md:h-90 md:inline-flex" src="https://melissataub.com/wp-content/uploads/2019/07/Twitter-Mockup-crop2.png" alt="logo"/>
       <div className="">
       {Object.values(providers).map((provider)=>(
           <div className="flex flex-col items-center">
-             <Image src={unitweet} alt="" width="90" height="90" className=" object-cover hoverEffect p-0 hover:bg-blue-100 xl:px-1 justify-center"/>
-             <p className="text-center text-sm italic my-10">Unitweet is a tweet sharing application</p>
+             <Image src={unitweet} alt="" width="190" height="190" className=" object-cover hoverEffect p-0 hover:bg-blue-100 xl:px-1 justify-center"/>
+             <p className="text-center text-lg my-10">Hello! <br/>Welcome to Unitweet</p>
              <button onClick={()=>signIn(provider.id,{callbackUrl:"/"})} className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500">Signin with {provider.name}</button>
            </div>
         ))}
