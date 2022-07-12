c481df564fa5541d22fe15deb1475979783809f4
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-12 20:29:05 +0530

    update feed and post section

--- components/Feed.js
+++ components/Feed.js
@@ -1,30 +1,23 @@
 import { SparklesIcon } from '@heroicons/react/outline'
-import React from 'react'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebase'
 import Input from './Input'
 import Post from './Post'
 
 function Feed() {
-    const posts = [
-        {
-            id:"1",
-            name:"Lalitha Vadavalli",
-            username:"lalitharamanaV",
-            userImg:"https://avatars.githubusercontent.com/u/58090261?v=4",
-            img:"https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1274&q=80",
-            text:"This is stunning",
-            timestamp:"1 hour ago"
-        },
-        {
-            id:"2",
-            name:"Lalitha ",
-            username:"lalithaV",
-            userImg:"https://avatars.githubusercontent.com/u/58090261?v=4",
-            img:"https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-            text:"Nice view",
-            timestamp:"2 hours ago"
-        }
-    ]
-  return (
+    const [posts,setPosts]=useState([]);
+    useEffect(
+        () => 
+        onSnapshot(
+            query(collection(db, "posts"), orderBy("timestamp", "desc")),
+            (snapshot) => {
+              setPosts(snapshot.docs);
+             }
+            ),
+            [] 
+           )
+    return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
      <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
      <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
