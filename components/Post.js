f177c57e2e969506a338785ee6061ef309fbc216
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-13 10:19:23 +0530

    added delete functionality

--- components/Post.js
+++ components/Post.js
@@ -2,9 +2,10 @@ import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, Trash
 import { collection, deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import Moment from 'react-moment'
-import { db } from '../firebase'
+import { db, storage } from '../firebase'
 import {HeartIcon as HeartIconFilled} from "@heroicons/react/solid"
 import {signIn, useSession} from "next-auth/react";
+import { deleteObject, ref } from 'firebase/storage'
 
 function Post({post}) {
   const {data:session} = useSession();
@@ -33,6 +34,13 @@ function Post({post}) {
         }
     }else{
       signIn()
+    }
+  }
+
+  async function deletePost() {
+    if(window.confirm('Are you sure you want to delete the post?')){
+      deleteDoc(doc(db, "posts", post.id))
+      deleteObject(ref(storage,`posts/${post.id}/image`))
     }
   }
 
@@ -65,7 +73,10 @@ function Post({post}) {
 
           <div className="flex justify-between text-gray-500 p-2">
             <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
-            <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"/>
+            {session?.user.uid === post?.data().id && (
+            <TrashIcon onClick={deletePost}className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"/>
+            )}
+            
             <div className="flex items-center">
             {hasLiked ? ( 
             <HeartIconFilled 
